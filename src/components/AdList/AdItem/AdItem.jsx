030ab5a4e1f032ac46d529487498bd8b30abf618
Samsonik91 <111237284+Samsonik91@users.noreply.github.com>
2022-08-19 18:05:55 +0300

    Update AdItem.jsx

--- src/components/AdList/AdItem/AdItem.jsx
+++ src/components/AdList/AdItem/AdItem.jsx
@@ -69,7 +69,7 @@ const AdItem = React.memo(({ad, disable, handleFavorite}) => {
                             <PriceText variant='h5'>{ad.price}</PriceText>
                             <Typography
                                 variant='subtitle1'
-                                sx={{marginTop: {xs: '18px', sm: 0}}}
+                                sx={{paddingTop: {xs: '18px', sm: 0}}}
                             >{ad.currency}</Typography>
                         </InnerPriceBox>
                         {ad.haggle ? <Typography variant='body2' component='div'>
