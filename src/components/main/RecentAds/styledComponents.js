913f5b70f92eeb7faa7930a729081d8cbfb07af4
Samsonik91 <111237284+Samsonik91@users.noreply.github.com>
2022-09-10 13:36:05 +0300

    Update styledComponents.js

--- src/components/main/RecentAds/styledComponents.js
+++ src/components/main/RecentAds/styledComponents.js
@@ -4,17 +4,14 @@ export const MainBox = styled(Box)(({theme)=>({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    marginTop: '16px',
-    [theme.breakpoints.down('sm')]:{
-        marginLeft: '16px
-    }
+    marginTop: '16px'
 }))
 
 export const AdsGrid = styled(Grid)(()=>({
     margin: 'auto',
     marginTop: '16px',
     rowGap: '16px',
-    width: '101%'
+    width: '102%'
 }))
 
 export const StyledGrid = styled(Grid)(({theme})=>({
