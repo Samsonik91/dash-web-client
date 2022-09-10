f1e6af4f729c5a21aa7fa8576210166713e59eb0
Samsonik91 <111237284+Samsonik91@users.noreply.github.com>
2022-09-10 12:43:06 +0300

    Update CategoryField.jsx

--- src/components/main/CategoryField.jsx
+++ src/components/main/CategoryField.jsx
@@ -23,7 +23,7 @@ const FieldBox = styled(Container)(({theme})=>({
     marginRight: '24px',
     borderRadius: '5px',
     [theme.breakpoints.down("md")]:{
-        width: '90%'
+        width: '70%'
     }
 }))
 
