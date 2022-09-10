574e17a4b21207a1e500ddb7b1bb1819455682d1
Samsonik91 <111237284+Samsonik91@users.noreply.github.com>
2022-09-10 15:30:10 +0300

    Update Slider.jsx

--- src/components/AdDetails/Slider/Slider.jsx
+++ src/components/AdDetails/Slider/Slider.jsx
@@ -26,7 +26,7 @@ const Slider = ({images}) => {
                     <img key={i+Math.random()} src={f}
                          style={{
                              position: 'absolute',
-                             objectFit: 'contain',
+                             objectFit: 'none',
                              width: '100%',
                              height: '100%',
                              opacity: imageNum === i ? 1 : 0,
