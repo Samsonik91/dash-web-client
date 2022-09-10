c85c29b9890696c83ab6a0d59f7f8a37f4023e80
Samsonik91 <111237284+Samsonik91@users.noreply.github.com>
2022-09-10 13:51:10 +0300

    Update styledComponents.js

--- src/components/main/RecentAds/styledComponents.js
+++ src/components/main/RecentAds/styledComponents.js
@@ -11,8 +11,8 @@ export const AdsGrid = styled(Grid)(()=>({
     margin: 'auto',
     marginTop: '16px',
     rowGap: '16px',
-    width: '100%',
-    paddingRight: '16px'
+    width: '101%',
+    paddingLeft: '16px'
 }))
 
 export const StyledGrid = styled(Grid)(({theme})=>({
