70bc8c908bf6a61b97d95ef174404928f717b864
Samsonik91 <111237284+Samsonik91@users.noreply.github.com>
2022-09-10 13:01:49 +0300

    Update RecentAds.jsx

--- src/components/main/RecentAds/RecentAds.jsx
+++ src/components/main/RecentAds/RecentAds.jsx
@@ -17,7 +17,7 @@ const RecentAds = ({blockWidth}) => {
     },[])
 
     return (
-        <MainBox sx={{width: `calc(${blockWidth} + 8px)`}}>
+        <MainBox sx={{width: blockWidth}}>
             <Typography variant='h5' sx={{textAlign: 'center'}}>
                 Последние добавленные объявления
             </Typography>
